b911a116639d9bb81dfe9f7d83348a23966b03d8
TANN1N <sonic100487@gmail.com>
2025-07-04 14:23:46 +0000

    FIX: 'in_bot_ready' 변수를 'is_bot_ready'로 수정

--- models/app_state.py
+++ models/app_state.py
@@ -4,7 +4,7 @@ import discord
 @dataclass
 class AppState:
     """애플리케이션의 모든 상태를 관리하는 데이터 클래스입니다."""
-    in_bot_ready: bool = False
+    is_bot_ready: bool = False
     current_guild: discord.Guild | None = None
     current_channel: discord.TextChannel | None = None
     all_guilds: list[discord.Guild] = field(default_factory=list)
