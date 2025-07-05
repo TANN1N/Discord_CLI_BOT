0d30ce50f49d001c3ccfa73180b7305a6bdf32ca
TANN1N <sonic100487@gmail.com>
2025-07-05 13:15:02 +0000

    새로운 이벤트 타입을 정의하는 EventType enum 클래스 추가

---
+++ core/event_types.py
@@ -0,0 +1,28 @@
+from enum import Enum, auto
+
+
+class EventType(Enum):
+    """
+    발행할 수 있는 이벤트 타입을 enum으로 명시적으로 지정함으로써 오타로 인한 오류를 방지.
+    IDE의 자동완성 기능을 사용할 수 있도록 함.
+    """
+    
+    # General Events
+    ERROR = auto()
+    SHOW_TEXT = auto()
+    
+    # Bot Status Events
+    BOT_READY = auto()
+    
+    # Guild/Server Events
+    GUILDS_UPDATED = auto()
+    GUILD_SELECTED = auto()
+    
+    # Channel Events
+    CHANNEL_SELECTED = auto()
+    
+    # Message Events
+    MESSAGES_UPDATED = auto()
+    MESSAGE_SENT_SUCCESS = auto()
+    FILE_SENT_SUCCESS = auto()
+    DISCORD_MESSAGE_RECEIVED = auto()
